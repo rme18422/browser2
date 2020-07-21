00bcf94ba94183b5b9b781b89857c05501a6ffbd
Waleed Mohsin <wikiwaleed2@gmail.com>
2020-07-22 01:31:38 +0500

    first Commit

---
+++ WebBrowserCSharp/WebBrowserCSharp/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WebBrowserCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.UrlTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.GoButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.HomeButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.BackButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.NextButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.RefreshButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.SaveButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator8 = new System.Windows.Forms.ToolStripSeparator();
+            this.PrintButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator7 = new System.Windows.Forms.ToolStripSeparator();
+            this.PrintPreviewButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.PropertiesButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, -263);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(630, 613);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.SandyBrown;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.UrlTextBox,
+            this.GoButton,
+            this.toolStripSeparator1,
+            this.HomeButton,
+            this.toolStripSeparator3,
+            this.BackButton,
+            this.toolStripSeparator2,
+            this.NextButton,
+            this.toolStripSeparator4,
+            this.RefreshButton,
+            this.toolStripSeparator6,
+            this.SaveButton,
+            this.toolStripSeparator8,
+            this.PrintButton,
+            this.toolStripSeparator7,
+            this.PrintPreviewButton,
+            this.toolStripSeparator5,
+            this.PropertiesButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(630, 25);
+            this.toolStrip1.TabIndex = 3;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(61, 22);
+            this.toolStripLabel1.Text = "Enter URL:";
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UrlTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(270, 25);
+            this.UrlTextBox.Text = "http://www.c-sharpcorner.com";
+            this.UrlTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UrlTextBox_KeyDown);
+            // 
+            // GoButton
+            // 
+            this.GoButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.GoButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.GoButton.Image = ((System.Drawing.Image)(resources.GetObject("GoButton.Image")));
+            this.GoButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(29, 22);
+            this.GoButton.Text = "GO";
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
+            this.HomeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(44, 22);
+            this.HomeButton.Text = "Home";
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Image = ((System.Drawing.Image)(resources.GetObject("BackButton.Image")));
+            this.BackButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(52, 22);
+            this.BackButton.Text = "Back";
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Image = ((System.Drawing.Image)(resources.GetObject("NextButton.Image")));
+            this.NextButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.NextButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(51, 22);
+            this.NextButton.Text = "Next";
+            this.NextButton.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.NextButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.RefreshButton.Image = ((System.Drawing.Image)(resources.GetObject("RefreshButton.Image")));
+            this.RefreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(50, 22);
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 25);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.SaveButton.Image = ((System.Drawing.Image)(resources.GetObject("SaveButton.Image")));
+            this.SaveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(35, 19);
+            this.SaveButton.Text = "Save";
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // toolStripSeparator8
+            // 
+            this.toolStripSeparator8.Name = "toolStripSeparator8";
+            this.toolStripSeparator8.Size = new System.Drawing.Size(6, 25);
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.PrintButton.Image = ((System.Drawing.Image)(resources.GetObject("PrintButton.Image")));
+            this.PrintButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.PrintButton.Name = "PrintButton";
+            this.PrintButton.Size = new System.Drawing.Size(36, 19);
+            this.PrintButton.Text = "Print";
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // toolStripSeparator7
+            // 
+            this.toolStripSeparator7.Name = "toolStripSeparator7";
+            this.toolStripSeparator7.Size = new System.Drawing.Size(6, 25);
+            // 
+            // PrintPreviewButton
+            // 
+            this.PrintPreviewButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.PrintPreviewButton.Image = ((System.Drawing.Image)(resources.GetObject("PrintPreviewButton.Image")));
+            this.PrintPreviewButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.PrintPreviewButton.Name = "PrintPreviewButton";
+            this.PrintPreviewButton.Size = new System.Drawing.Size(80, 19);
+            this.PrintPreviewButton.Text = "Print Preview";
+            this.PrintPreviewButton.Click += new System.EventHandler(this.PrintPreviewButton_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 25);
+            // 
+            // PropertiesButton
+            // 
+            this.PropertiesButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.PropertiesButton.Image = ((System.Drawing.Image)(resources.GetObject("PropertiesButton.Image")));
+            this.PropertiesButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.PropertiesButton.Name = "PropertiesButton";
+            this.PropertiesButton.Size = new System.Drawing.Size(64, 19);
+            this.PropertiesButton.Text = "Properties";
+            this.PropertiesButton.Click += new System.EventHandler(this.PropertiesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 350);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox UrlTextBox;
+        private System.Windows.Forms.ToolStripButton GoButton;
+        private System.Windows.Forms.ToolStripButton HomeButton;
+        private System.Windows.Forms.ToolStripButton BackButton;
+        private System.Windows.Forms.ToolStripButton NextButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton RefreshButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripButton SaveButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator8;
+        private System.Windows.Forms.ToolStripButton PrintButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator7;
+        private System.Windows.Forms.ToolStripButton PrintPreviewButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripButton PropertiesButton;
+    }
+}
+
---
+++ WebBrowserCSharp/WebBrowserCSharp/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebBrowserCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Implement keypress on TextBox and display the URL in the browser        
+        private void UrlTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                OpenURLInBrowser(UrlTextBox.Text);
+            }
+        }
+
+// GO button click event handler. 
+private void GoButton_Click(object sender, EventArgs e)
+{
+    if (String.IsNullOrEmpty(UrlTextBox.Text) ||
+        UrlTextBox.Text.Equals("about:blank"))
+    {
+        MessageBox.Show("Enter a valid URL.");
+        UrlTextBox.Focus();
+        return;
+    }
+    OpenURLInBrowser(UrlTextBox.Text);         
+}
+
+private void OpenURLInBrowser(string url)
+{        
+    if (!url.StartsWith("http://") &&
+        !url.StartsWith("https://"))
+    {
+        url = "http://" + url;
+    }
+    try
+    {
+        webBrowser1.Navigate(new Uri(url));
+    }
+    catch (System.UriFormatException)
+    {
+        return;
+    }
+}
+
+// Home button takes user home 
+private void HomeButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.GoHome();
+}
+
+// Go back
+private void BackButton_Click(object sender, EventArgs e)
+{
+    if (webBrowser1.CanGoBack)
+        webBrowser1.GoBack();
+}
+
+// Next 
+private void NextButton_Click(object sender, EventArgs e)
+{
+    if (webBrowser1.CanGoForward)
+        webBrowser1.GoForward();
+}       
+
+// Refresh
+private void RefreshButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.Refresh();
+}
+
+// Save button launches SaveAs dialog
+private void SaveButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.ShowSaveAsDialog();
+}
+
+// PrintPreview button launches PrintPreview dialog
+private void PrintPreviewButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.ShowPrintPreviewDialog();
+}
+
+// Properties button
+private void PropertiesButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.ShowPropertiesDialog();
+}
+
+// Show Print dialog
+private void PrintButton_Click(object sender, EventArgs e)
+{
+    webBrowser1.ShowPrintDialog();
+}
+    }
+}
